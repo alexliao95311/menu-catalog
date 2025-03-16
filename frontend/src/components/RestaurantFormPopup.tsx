a479cb31355326ea1093c784d5c6a28cdd50e746
alex l <alexliao365@gmail.com>
2025-03-16 13:01:04 -0700

    fix close button

--- frontend/src/components/RestaurantFormPopup.tsx
+++ frontend/src/components/RestaurantFormPopup.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import RestaurantForm from './RestaurantForm';
-
-interface RestaurantFormPopupProps {
-  initialData: {
-    name: string;
-    description: string;
-    menu_items: { name: string; description: string; price: string }[];
-  };
-  onClose: () => void;
-  onRestaurantAdded: () => void;
-}
-
-export default function RestaurantFormPopup({ initialData, onClose, onRestaurantAdded }: RestaurantFormPopupProps) {
-  return (
-    <div className="popup-overlay">
-      <div className="popup-content">
-        <RestaurantForm onRestaurantAdded={onRestaurantAdded} initialData={initialData} />
-      </div>
-    </div>
-  );
-}
+ import RestaurantForm from './RestaurantForm';
+ 
+ interface RestaurantFormPopupProps {
+   initialData: {
+     name: string;
+     description: string;
+     menu_items: { name: string; description: string; price: string }[];
+   };
+   onClose: () => void;
+   onRestaurantAdded: () => void;
+ }
+ 
+ export default function RestaurantFormPopup({ initialData, onClose, onRestaurantAdded }: RestaurantFormPopupProps) {
+   return (
+     <div className="popup-overlay">
+       <div className="popup-content">
+         <button onClick={onClose} className="close-button">X</button>
+         <RestaurantForm onRestaurantAdded={onRestaurantAdded} initialData={initialData} />
+       </div>
+     </div>
+   );
+ }
